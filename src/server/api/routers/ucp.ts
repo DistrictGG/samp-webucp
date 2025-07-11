d2e11562573e4dc98a84714e4b3e7e5499045254
Galang Indra <51304312+galangindraa@users.noreply.github.com>
2025-07-11 17:45:31 +0700

    adjust api  ucp / karakter

--- src/server/api/routers/ucp.ts
+++ src/server/api/routers/ucp.ts
@@ -4,66 +4,67 @@ import { createTRPCRouter, protectedProcedure, publicProcedure} from "~/server/a
 
 export const ucpRouter = createTRPCRouter({
   playerucp: protectedProcedure
-    .query(async ({ ctx }) => {
-      const playerucp = await ctx.db.characterUcp.findUnique({
-        where: {
-          userid: ctx.session.user.id,
-        },
-        select: {
-          id: true,
-          ucp: true,
-          active: true,
-          codeverify: true,
-          coin: true,
-        }
-      });
-      return playerucp ?? null;
-    }),
+  .query(async ({ ctx }) => {
+    const playerucp = await ctx.db.characterUcp.findUnique({
+      where: {
+        userid: ctx.session.user.id,
+      },
+      select: {
+        id: true,
+        ucp: true,
+        active: true,
+        codeverify: true,
+        coin: true,
+      }
+    });
+    return playerucp ?? null;
+  }),
 
   CreateUcp: protectedProcedure
-    .input(z.object({ username: z.string().min(3).max(20) }))
-    .mutation(async ({ ctx, input }) => {
-      const existingUcp = await ctx.db.characterUcp.findUnique({
-        where: {
-          userid: ctx.session.user.id,
-        },
-      });
+  .input(z.object({ username: z.string().min(3).max(20) }))
+  .mutation(async ({ ctx, input }) => {
+    const existingUcp = await ctx.db.characterUcp.findUnique({
+      where: {
+        userid: ctx.session.user.id,
+      },
+    });
 
-      if (existingUcp) {
-        throw new Error("UCP already exists for this user.");
+    if (existingUcp) {
+      throw new Error("UCP already exists for this user.");
+    }
+
+    const codeverify = Math.floor(100000 + Math.random() * 900000);
+
+    const newUcp = await ctx.db.characterUcp.create({
+      data: {
+        userid: ctx.session.user.id,
+        ucp: input.username,
+        active: 0,
+        codeverify: codeverify,
+        coin: 0,
+      },
+    });
+
+    return newUcp;
+  }),
+
+  getCharacter: protectedProcedure
+  .query(async ({ ctx }) => {
+    const character = await ctx.db.character.findMany({
+      where: {
+        uuid: ctx.session.user.id,
+      },
+      select: {
+        id: true,
+        name: true,
+        skin: true,
+        level: true,
+        levelup: true,
+        money: true,
+        bankmoney: true,
+        vip: true,
       }
-
-      const codeverify = Math.floor(100000 + Math.random() * 900000);
-
-      const newUcp = await ctx.db.characterUcp.create({
-        data: {
-          userid: ctx.session.user.id,
-          ucp: input.username,
-          active: 0,
-          codeverify: codeverify,
-          coin: 0,
-        },
-      });
-
-      return newUcp;
-    })
-
-  // getCharacter: protectedProcedure
-  // .input(z.object({ uuid: z.string()}))
-  // .query(async ({ ctx, input }) => {
-  //   const character = await ctx.db.character.findMany({
-  //     where: {
-  //       uuid: input.uuid,
-  //     },
-  //     select: {
-  //       id: true,
-  //       name: true,
-  //       level: true,
-  //       levelup: true,
-  //       job: true,
-  //       money: true,
-  //     }
-  //   });
-  //   return character ?? null;
-  // }),
+    });
+    return character ?? null;
+  }),
 });
